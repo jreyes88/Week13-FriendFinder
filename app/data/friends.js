63d9bac5d89ea82bd0bce23fac4f399e97be3bc5
Joey Reyes <jreyes88@gmail.com>
2016-07-22 16:29:07 -0500

    initial commit, logic looks like it is working, going for front end design now

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+	{
+    	"name": "Jimmy Buffett",
+    	"photo": "http://www.jztours.com/wp-content/uploads/2014/05/jimmy-buffett-4f3a83f5199eb.jpg",
+    	"scores": [
+	        5,
+	        5,
+	        5,
+	        5,
+	        5,
+	        5,
+	        5,
+	        5,
+	        5,
+	        5
+	    ]
+	},
+	{
+		"name": "Don Henley",
+		"photo": "https://i.ytimg.com/vi/CQNH-qJMgFw/maxresdefault.jpg",
+		"scores": [
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4
+		]
+	},
+	{
+		"name": "Joe Walsh",
+		"photo": "https://i.ytimg.com/vi/p0Iq-UBWKcU/maxresdefault.jpg",
+		"scores": [
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3
+		]
+	},
+	{
+		"name": "John Fogerty",
+		"photo": "https://www.zealnyc.com/wp-content/uploads/2015/05/JohnFogerty1.jpg",
+		"scores": [
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2
+		]
+	},
+	{
+		"name": "Steve Miller",
+		"photo": "http://www.stevemillerband.com/images/double-neck-mini.jpg",
+		"scores": [
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+		]
+	},
+	{
+		"name": "Peter Frampton",
+		"photo": "http://img.wennermedia.com/featured-promo-580/rs-130772-20140331-frampton-x1800-1396299285.jpg",
+		"scores": [
+			1,
+			2,
+			3,
+			4,
+			5,
+			5,
+			4,
+			3,
+			2,
+			1
+		]
+	},
+	{
+		"name": "Danny McBride",
+		"photo": "http://cdn2.holytaco.com/wp-content/uploads/2012/02/kenny_powers_wig.jpg",
+		"scores": [
+			2,
+			3,
+			5,
+			1,
+			4,
+			3,
+			4,
+			2,
+			5,
+			1
+		]
+	},
+	{
+		"name": "David Hasselhoff",
+		"photo": "http://static.socialitelife.com/uploads/2009/05/davidhasselhoff-drunk-photos-05032009-05-400x300.jpg",
+		"scores": [
+			5,
+			4,
+			5,
+			2,
+			5,
+			2,
+			3,
+			5,
+			4,
+			2
+		]
+	},
+	{
+		"name": "Adam Sandler",
+		"photo": "http://www.multichannel.com/sites/default/files/public/styles/blog_content/public/Adam-Sandler.jpg?itok=FA5m_Owf",
+		"scores": [
+			2,
+			5,
+			3,
+			4,
+			1,
+			5,
+			4,
+			2,
+			5,
+			1
+		]
+	},
+	{
+		"name": "Dolly Parton",
+		"photo": "http://dollypartonfans.co.uk/wordpress/wp-content/uploads/2013/02/Dolly_Parton400x300.jpg",
+		"scores": [
+			5,
+			3,
+			4,
+			2,
+			5,
+			4,
+			5,
+			3,
+			4,
+			3
+		]
+	}
+
+]
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,29 @@
+var bodyParser = require('body-parser');
+var path = require('path');
+var friendData = require('../data/friends.js');
+
+module.exports = function(app) {
+    // Get method to retrieve
+    app.get("/api/friends", function(req,res) {
+        res.json(friendData);
+    })
+    app.post("/api/friends", function (req, res) {
+        var lastDiff = 50;
+        var bestMatch;
+        friendData.forEach(function(item) {
+            // console.log(item);
+            var diff = 0;
+            for(i=0;i<item.scores.length;i++) {
+                diff += Math.abs(item.scores[i] - req.body.scores[i]);
+            }
+            if (diff <= lastDiff) {
+                lastDiff = diff;
+                bestMatch = item;
+            }
+        })
+
+        // console.log(bestMatch);
+        res.json(bestMatch);
+        friendData.push(req.body);
+    })
+}
