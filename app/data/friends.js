37d72df0c5e2c8967f0ceb9b698d68bf8544a181
Joey Reyes <jreyes88@gmail.com>
2016-07-23 12:46:59 -0500

    almost done

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,7 @@ var friends = [
 	{
     	"name": "Jimmy Buffett",
     	"photo": "http://www.thetelegram.com/media/photos/unis/2013/11/22/2013-11-22-09-17-44-B1-max-cmykjimmy-buffett.jpg",
+    	"resultText": "You got Jimmy Buffett! Wow! You're a real parrothead!!!",
     	"scores": [
 	        5,
 	        5,
@@ -18,6 +19,7 @@ var friends = [
 	{
 		"name": "Don Henley",
 		"photo": "http://images1.dallasobserver.com/imager/u/original/7680995/henley_0202_david_mcclister.jpg",
+		"resultText": "You got Don Henley! You got your hair combed back and your sunglasses on, baby!!",
 		"scores": [
 			4,
 			4,
@@ -34,6 +36,7 @@ var friends = [
 	{
 		"name": "Joe Walsh",
 		"photo": "http://i2.cdn.turner.com/cnnnext/dam/assets/160421073552-joe-walsh-file-super-169.jpg",
+		"resultText": "You got Joe Walsh!! You don't need the ladies crying because the story's sad :(",
 		"scores": [
 			3,
 			3,
@@ -50,6 +53,7 @@ var friends = [
 	{
 		"name": "John Fogerty",
 		"photo": "http://images6.fanpop.com/image/photos/36800000/John-Fogerty-creedence-clearwater-revival-36880030-1920-1080.jpg",
+		"resultText": "Oh, you got John Fogerty. You were born on the bayou, and you like to spend your time chooglin.",
 		"scores": [
 			2,
 			2,
@@ -66,6 +70,7 @@ var friends = [
 	{
 		"name": "Steve Miller",
 		"photo": "http://assets.klipsch.com/herobox-images/Rock-Hall-2016-Steve-Miller.jpg",
+		"resultText": "Easy there space cowboy, you got Steve Miller. Clearly you're a joker/smoker/midnight toker, etc.",
 		"scores": [
 			1,
 			1,
@@ -81,7 +86,8 @@ var friends = [
 	},
 	{
 		"name": "Peter Frampton",
-		"photo": "./images/artists/peter_frampton.jpg",
+		"photo": "http://songsyouresickof.com/blog/wp-content/uploads/2015/08/peter_frampton9.jpg",
+		"resultText": "You got Peter Frampton! You feel like you do.",
 		"scores": [
 			1,
 			2,
@@ -97,7 +103,8 @@ var friends = [
 	},
 	{
 		"name": "Christopher Cross",
-		"photo": "./images/artists/christopher_cross.jpg",
+		"photo": "http://www.aceshowbiz.com/images/wennpic/wenn3178999.jpg",
+		"resultText": "Sailing takes you away, because clearly you are Christopher Cross.",
 		"scores": [
 			5,
 			4,
@@ -113,7 +120,8 @@ var friends = [
 	},
 	{
 		"name": "Kenny Loggins",
-		"photo": "./images/artists/kenny_loggins.jpg",
+		"photo": "http://www.urabanchou.com/images/2010/08/51/img015.jpg",
+		"resultText": "What's up Kenny Loggins, now you gotta cut loose, Footloose, kick off the Sunday shoes!",
 		"scores": [
 			1,
 			3,
@@ -129,7 +137,8 @@ var friends = [
 	},
 	{
 		"name": "John Oates",
-		"photo": "./images/artists/john_oates.jpg",
+		"photo": "http://www.mtv.com/shared/promoimages/bands/o/oates_john/JohnOates.jpg",
+		"resultText": "When you feel cold, I'll warm you, and when you feel you can't go on, I'll come and hold you. You're John Oates!!",
 		"scores": [
 			2,
 			5,
@@ -145,7 +154,8 @@ var friends = [
 	},
 	{
 		"name": "Michael Bolton",
-		"photo": "./images/artists/michael_bolton.jpg",
+		"photo": "https://i.ytimg.com/vi/BIb4G8Z2E2Y/maxresdefault.jpg",
+		"resultText": "When a man loves a woman, you give everything you got because you're Michael Bolton",
 		"scores": [
 			5,
 			3,
