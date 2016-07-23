532e23f7f0d358c6c194e61c37ae6b3e7329dea5
Joey Reyes <jreyes88@gmail.com>
2016-07-23 12:57:57 -0500

    Almost finished with the front end design

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friends = [
 	{
-    	"name": "Jimmy Buffett",
-    	"photo": "http://www.thetelegram.com/media/photos/unis/2013/11/22/2013-11-22-09-17-44-B1-max-cmykjimmy-buffett.jpg",
-    	"resultText": "You got Jimmy Buffett! Wow! You're a real parrothead!!!",
-    	"scores": [
+    	'name': 'Jimmy Buffett',
+    	'photo': 'http://www.thetelegram.com/media/photos/unis/2013/11/22/2013-11-22-09-17-44-B1-max-cmykjimmy-buffett.jpg',
+    	'resultText': "You got Jimmy Buffett! Wow! You're a real parrothead!!!",
+    	'scores': [
 	        5,
 	        5,
 	        5,
@@ -17,10 +17,10 @@ var friends = [
 	    ]
 	},
 	{
-		"name": "Don Henley",
-		"photo": "http://images1.dallasobserver.com/imager/u/original/7680995/henley_0202_david_mcclister.jpg",
-		"resultText": "You got Don Henley! You got your hair combed back and your sunglasses on, baby!!",
-		"scores": [
+		'name': 'Don Henley',
+		'photo': 'http://images1.dallasobserver.com/imager/u/original/7680995/henley_0202_david_mcclister.jpg',
+		'resultText': 'You got Don Henley! You got your hair combed back and your sunglasses on, baby!!',
+		'scores': [
 			4,
 			4,
 			4,
@@ -34,10 +34,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Joe Walsh",
-		"photo": "http://i2.cdn.turner.com/cnnnext/dam/assets/160421073552-joe-walsh-file-super-169.jpg",
-		"resultText": "You got Joe Walsh!! You don't need the ladies crying because the story's sad :(",
-		"scores": [
+		'name': 'Joe Walsh',
+		'photo': 'http://i2.cdn.turner.com/cnnnext/dam/assets/160421073552-joe-walsh-file-super-169.jpg',
+		'resultText': "You got Joe Walsh!! You don't need the ladies crying because the story's sad :(",
+		'scores': [
 			3,
 			3,
 			3,
@@ -51,10 +51,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "John Fogerty",
-		"photo": "http://images6.fanpop.com/image/photos/36800000/John-Fogerty-creedence-clearwater-revival-36880030-1920-1080.jpg",
-		"resultText": "Oh, you got John Fogerty. You were born on the bayou, and you like to spend your time chooglin.",
-		"scores": [
+		'name': 'John Fogerty',
+		'photo': 'http://images6.fanpop.com/image/photos/36800000/John-Fogerty-creedence-clearwater-revival-36880030-1920-1080.jpg',
+		'resultText': 'Oh, you got John Fogerty. You were born on the bayou, and you like to spend your time chooglin.',
+		'scores': [
 			2,
 			2,
 			2,
@@ -68,10 +68,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Steve Miller",
-		"photo": "http://assets.klipsch.com/herobox-images/Rock-Hall-2016-Steve-Miller.jpg",
-		"resultText": "Easy there space cowboy, you got Steve Miller. Clearly you're a joker/smoker/midnight toker, etc.",
-		"scores": [
+		'name': 'Steve Miller',
+		'photo': 'http://assets.klipsch.com/herobox-images/Rock-Hall-2016-Steve-Miller.jpg',
+		'resultText': "Easy there space cowboy, you got Steve Miller. Clearly you're a joker/smoker/midnight toker, etc.",
+		'scores': [
 			1,
 			1,
 			1,
@@ -85,10 +85,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Peter Frampton",
-		"photo": "http://songsyouresickof.com/blog/wp-content/uploads/2015/08/peter_frampton9.jpg",
-		"resultText": "You got Peter Frampton! You feel like you do.",
-		"scores": [
+		'name': 'Peter Frampton',
+		'photo': 'http://songsyouresickof.com/blog/wp-content/uploads/2015/08/peter_frampton9.jpg',
+		'resultText': 'You got Peter Frampton! You feel like you do.',
+		'scores': [
 			1,
 			2,
 			3,
@@ -102,10 +102,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Christopher Cross",
-		"photo": "http://www.aceshowbiz.com/images/wennpic/wenn3178999.jpg",
-		"resultText": "Sailing takes you away, because clearly you are Christopher Cross.",
-		"scores": [
+		'name': 'Christopher Cross',
+		'photo': 'http://www.aceshowbiz.com/images/wennpic/wenn3178999.jpg',
+		'resultText': 'Sailing takes you away, because clearly you are Christopher Cross.',
+		'scores': [
 			5,
 			4,
 			3,
@@ -119,10 +119,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Kenny Loggins",
-		"photo": "http://www.urabanchou.com/images/2010/08/51/img015.jpg",
-		"resultText": "What's up Kenny Loggins, now you gotta cut loose, Footloose, kick off the Sunday shoes!",
-		"scores": [
+		'name': 'Kenny Loggins',
+		'photo': 'http://www.urabanchou.com/images/2010/08/51/img015.jpg',
+		'resultText': "What's up Kenny Loggins, now you gotta cut loose, Footloose, kick off the Sunday shoes!",
+		'scores': [
 			1,
 			3,
 			5,
@@ -136,10 +136,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "John Oates",
-		"photo": "http://www.mtv.com/shared/promoimages/bands/o/oates_john/JohnOates.jpg",
-		"resultText": "When you feel cold, I'll warm you, and when you feel you can't go on, I'll come and hold you. You're John Oates!!",
-		"scores": [
+		'name': 'John Oates',
+		'photo': 'http://www.mtv.com/shared/promoimages/bands/o/oates_john/JohnOates.jpg',
+		'resultText': "When you feel cold, I'll warm you, and when you feel you can't go on, I'll come and hold you. You're John Oates!!",
+		'scores': [
 			2,
 			5,
 			3,
@@ -153,10 +153,10 @@ var friends = [
 		]
 	},
 	{
-		"name": "Michael Bolton",
-		"photo": "https://i.ytimg.com/vi/BIb4G8Z2E2Y/maxresdefault.jpg",
-		"resultText": "When a man loves a woman, you give everything you got because you're Michael Bolton",
-		"scores": [
+		'name': 'Michael Bolton',
+		'photo': 'https://i.ytimg.com/vi/BIb4G8Z2E2Y/maxresdefault.jpg',
+		'resultText': "When a man loves a woman, you give everything you got because you're Michael Bolton",
+		'scores': [
 			5,
 			3,
 			4,
@@ -169,7 +169,6 @@ var friends = [
 			3
 		]
 	}
-
 ]
 
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,15 +3,13 @@ var path = require('path');
 var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
-    // Get method to retrieve
-    app.get("/api/friends", function(req,res) {
+    app.get('/api/friends', function(req,res) {
         res.json(friendData);
-    })
-    app.post("/api/friends", function (req, res) {
+    });
+    app.post('/api/friends', function (req, res) {
         var lastDiff = 50;
         var bestMatch;
         friendData.forEach(function(item) {
-            // console.log(item);
             var diff = 0;
             for(i=0;i<item.scores.length;i++) {
                 diff += Math.abs(item.scores[i] - req.body.scores[i]);
@@ -21,9 +19,7 @@ module.exports = function(app) {
                 bestMatch = item;
             }
         })
-
-        // console.log(bestMatch);
         res.json(bestMatch);
         friendData.push(req.body);
-    })
-}
+    });
+};
